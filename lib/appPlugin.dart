2432bc59f21516afaa6dbdb0ed492b1446a4669b
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-09-09 20:10:29 +0530

    flutter module added to native android

---
+++ lib/appPlugin.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+const platform = const MethodChannel('com.example.flutter_module.host');
+
+String counter = '';
+
+Future<String?> getFromNative(MethodCall methodCall) async {
+  debugPrint("Future getFromNative");
+  try {
+    if (methodCall.method == "getFromNative") {
+      String arg = methodCall.arguments;
+      debugPrint("getFromNative $arg");
+      Map<String, dynamic> map = jsonDecode(arg);
+      counter = map['counter'];
+
+      return counter;
+    }
+  } catch (e) {
+    print("getFromNative $e");
+  }
+  return null;
+}
